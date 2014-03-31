f3c0f23656c571227aa4ec55a0ceecad7b9053ec
GOAB <project_rawr@hotmail.com>
2014-03-31 14:45:27 +0200

    New project

---
+++ RetriPala/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace RetriPala
+{
+	[Activity (Label = "RetriPala", MainLauncher = false)]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks!", count++);
+			};
+		}
+	}
+}
+
+namespace SplashScreen
+{
+	using System.Threading;
+
+	using Android.App;
+	using Android.OS;
+
+	[Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+	public class SplashActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			Thread.Sleep(1000); // Simulate a long loading process on app startup.
+			StartActivity(typeof(Activity));
+		}
+	}
+}
+
+
